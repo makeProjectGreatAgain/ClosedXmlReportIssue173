f353dbecb03725c06940e79b2672937bb7be97a6
nMatveev <nikolayMatveev.job@gmail.com>
2020-06-30 20:36:18 +0300

    Добавьте файлы проекта.

---
+++ ClosedXmlReportBug173LastRowGoesLeft/Program.cs
@@ -0,0 +1,26 @@
+﻿using ClosedXML.Report;
+using System.Diagnostics;
+
+namespace ClosedXmlReportBug173LastRowGoesLeft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Report();
+        }
+
+        public static void Report()
+        {
+            const string outputFile = @".\Output\resultBugWithLastRow.xlsx";
+            var template = new XLTemplate(@".\Templates\BugWithLastRow.xlsx");
+            var reportData = ReportData.GetExample();
+            template.AddVariable(reportData);
+            template.Generate();
+            template.SaveAs(outputFile);
+
+            //Show report
+            Process.Start(new ProcessStartInfo(outputFile) { UseShellExecute = true });
+        }
+    }
+}
---
+++ ClosedXmlReportBug173LastRowGoesLeft/ReportData.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace ClosedXmlReportBug173LastRowGoesLeft
+{
+    public class ReportData
+    {
+        public int Id { get; set; }
+        public IList<Customer> Customers { get; set; }
+        public static ReportData GetExample()
+        {
+            var reportData = new ReportData();
+            reportData.Id = 2;
+            reportData.Customers = new List<Customer>();
+            var a = new Customer() { Name = "qwe", Balance = "234", SomeSpecInfo = "zzz" };
+            reportData.Customers.Add(a);
+            reportData.Customers.Add(a);
+
+            foreach (var customer in reportData.Customers)
+            {
+                customer.Orders = new List<Order>();
+                var b = new Order() { Number = "777", Price = "123" };
+                customer.Orders.Add(b);
+                customer.Orders.Add(b);
+            }
+            return reportData;
+        }
+    }
+
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Balance { get; set; }
+        public string SomeSpecInfo { get; set; }
+
+        public IList<Order> Orders { get; set; }
+    }
+    public class Order
+    {
+        public string Number { get; set; }
+        public string Price { get; set; }
+    }
+}
